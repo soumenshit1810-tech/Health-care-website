9352238e44dc89be456769e7fb98facfcc6e47ca
soumenshit1810-tech <soumenshit1810@gmail.com>
2025-10-21 21:32:59 +0530

    backend

---
+++ server/controller/user.controller.js
@@ -0,0 +1,19 @@
+const User = require("../models/user.model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+exports.registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password)
+    return res.status(400).json({ message: "All fields required" });
+
+  const userExists = await User.findOne({ email });
+  if (userExists)
+    return res.status(400).json({ message: "User already exists" });
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = await User.create({ name, email, password: hashedPassword });
+
+  res.json({ message: "User registered successfully", user });
+};
