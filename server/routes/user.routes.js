a5e2a1ec2ca98276ae88021dc34b50c3f9c5f8a0
soumenshit1810-tech <soumenshit1810@gmail.com>
2025-10-27 23:54:01 +0530

    changes

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,7 +1,13 @@
-const express = require("express");
-const router = express.Router();
-const { registerUser } = require("../controllers/user.controller");
+const express=require('express')
+const {userLogin, userRegister,userProfile}=require('../controller/user.controller')
+const {auth}=require('../middleware/auth.middleware')
+const router=express.Router()
 
-router.post("/register", registerUser);
 
-module.exports = router;
+router.post("/login",userLogin)
+router.post("/register",userRegister)
+router.get("/profile", auth ,userProfile)
+// router.get("/my-wishlist",userRegister)
+// router.get("/my-orders",userRegister)
+
+module.exports =router
